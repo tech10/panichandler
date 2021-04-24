d3dbb61bf1e6266b6f19b89559a0b92ee06dcb42
Blake G <blakeblind@gmail.com>
2021-04-24 12:54:32 -0600

    Test Capture struct.

---
+++ capture_test.go
@@ -0,0 +1,68 @@
+package panic_handler
+
+import (
+	"context"
+	"sync"
+	"testing"
+)
+
+type captT struct {
+	f func()
+}
+
+func (c *captT) DoPanicTask(i *Info) {
+	c.f()
+}
+
+func Test_Capture(t *testing.T) {
+	var l sync.Mutex
+	var wg sync.WaitGroup
+	logstr := ""
+	ctx, ctxc := context.WithCancel(context.Background())
+	wg.Add(1)
+	go func() {
+		<-ctx.Done()
+		l.Lock()
+		defer l.Unlock()
+		logstr += "Context canceled."
+		wg.Done()
+	}()
+	c := NewCapture()
+	c.CC = ctxc
+	c.C = make(chan *Info)
+	wg.Add(1)
+	go func() {
+		<-c.C
+		l.Lock()
+		defer l.Unlock()
+		logstr += "Channel received data.\n"
+		wg.Done()
+	}()
+	wg.Add(1)
+	c.F = func(i *Info) {
+		l.Lock()
+		defer l.Unlock()
+		logstr += "Function called.\n"
+		wg.Done()
+	}
+	wg.Add(1)
+	c.T = &captT{
+		f: func() {
+			l.Lock()
+			defer l.Unlock()
+			logstr += "Task executed.\n"
+			wg.Done()
+		},
+	}
+	go func() {
+		defer c.Catch()
+		panic("testing capture struct")
+	}()
+	wg.Wait()
+	l.Lock()
+	defer l.Unlock()
+	if logstr == "" {
+		t.Fatal("Nothing was executed, but should have been.")
+	}
+	t.Log(logstr)
+}
